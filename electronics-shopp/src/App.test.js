9955b8abd5f00c7ce5c0a55e30c65d80f9c9ce34
yumvm <shostakdmitriy11@gmail.com>
2026-01-14 00:30:59 +0300

    Revert "first commit"
    This reverts commit 1ebc079b25e7b80e3332938c7d7f9c1cb523b1df.

--- electronics-shopp/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app header', () => {
-  render(<App />);
-  const header = screen.getByText(/Electronics Shop/i);
-  expect(header).toBeInTheDocument();
-});
--- electronics-shopp/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Footer.module.css";
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      © 2025 Electronics Shop
-    </footer>
-  );
-}
